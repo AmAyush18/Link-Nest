5422e7d1dd1a390207ae6b6140759dd4245e1f17
Ayush Dwivedi <amayush0818@gmail.com>
2024-01-05 23:17:54 +0530

    Completed Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='w-[100%] bg-cream py-2'>
+      <div className="w-[80%] m-auto flex flex-col gap-2">
+        <p className='text-xs text-center text-blue'>&#169;	2024 <span className='text-orange'>link</span><span className='text-blue fond-semibold'>nest</span></p>
+        <p className='text-xs text-center text-orange'>Made with 💙 in Bharat</p>
+      </div>
+    </div>
   )
 }
 
