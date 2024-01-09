6a9f84e6d2b7f72329921fd28547e51759f25ee3
Ayush Dwivedi <amayush0818@gmail.com>
2024-01-09 11:13:54 +0530

    Solved Routing Bug using HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
-
 import Layout from './Layout'
 import { About, Collections, Home, Login, Signup } from './components'
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<Layout />}>
-      <Route path='' element={<Home />} />
-      <Route path='about' element={<About />} />
-      <Route path='collections' element={<Collections />} />
-      <Route path='login' element={<Login />} />
-      <Route path='signup' element={<Signup />} />
-    </Route>
-  )
-)
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+
+const Root = () => (
+  <Router>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route path='' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='collections' element={<Collections />} />
+        <Route path='login' element={<Login />} />
+        <Route path='signup' element={<Signup />} />
+      </Route>
+    </Routes>
+  </Router>
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <Root />
 )
